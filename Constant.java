69a8fd94b354446f978ee662ac1cd280ce2d904c
li-fengjie <li-fengjie@qq.com>
2017-08-25 22:04:34 +0800

    Create Constant.java

---
+++ Constant.java
@@ -0,0 +1,22 @@
+package cn.game.util;
+
+/**
+ * 开发中用到的常量
+ * @author LiFen
+ *
+ */
+public class Constant {
+	public static final int GAME_WIDTH = 455;
+	public static final int GAME_HEIGHT = 810;
+	public static final int boardWidth = 100;
+	public static final int boardHeight = 30;
+	public static final int boardSpeed = 20;
+	public static final int primaryBallSpeed = 12;
+	public static final int intermediateBallSpeed = 18;
+	public static final int advancedBallSpeed = 35;
+	public static final int ballWidth = 30;
+	public static final int ballHeight = 30;
+	public static final int blockWidth = 30;
+	public static final int blockHeight = 30;
+	public static final int num = 20;
+}
